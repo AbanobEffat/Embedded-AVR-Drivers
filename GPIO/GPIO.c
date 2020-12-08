0f60597480a01ffde7a0d788dea36ca732dfea87
AbanobEffat <abanob79@gmail.com>
2020-12-08 17:25:24 +0200

    Initial commit

---
+++ GPIO/GPIO.c
@@ -0,0 +1,17 @@
+/*
+ * GPIO.c
+ *
+ * Created: 12/8/2020 5:11:26 PM
+ *  Author: Abanob
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ GPIO/macros.h
@@ -0,0 +1,16 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 12/8/2020 5:11:59 PM
+ *  Author: Abanob
+ */ 
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+
+
+
+
+#endif /* INCFILE1_H_ */
